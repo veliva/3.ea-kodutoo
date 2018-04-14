13a4549c24092c2349dd1c5f33eea5a5e130d322
Veli Vaigur <31840289+veliva@users.noreply.github.com>
2018-04-14 23:48:21 +0300

    ükshaaval kustutamine
    Ükshaaval lisamine, ükshaaval kustutamine ja kõik korraga kustutamine töötab. Praegused aadressid font muudetud.

--- options.js
+++ options.js
@@ -1,33 +1,57 @@
 window.onload = function(){
     document.getElementById("addBtn").addEventListener("click", addURLstorage);
     showHighScores();
+    document.getElementById("clearAllBtn").addEventListener("click", clearStorage);
+    document.getElementById("removeBtn").addEventListener("click", removeURLstorage);
 };
 
-function addURLstorage(){
-    var pageUrl = document.getElementById("addURL").value;
-    console.log("salvestatud: " + pageUrl);
-    saveScore(pageUrl);
+function clearStorage () {
+    window.localStorage.clear();
+    showHighScores();
 }
 
-function saveScore (pageUrl) {
+function addURLstorage(){
+    let newUrl = document.getElementById("addURL").value;
+    saveScore(newUrl);
+}
+
+function removeURLstorage(){
+    let newUrl = document.getElementById("removeURL").value;
+    deleteUrl(newUrl);
+}
+
+function saveScore (newUrl) {
     arr = []
     if (window.localStorage.length == 0) {
-        url = [pageUrl]
+        url = [newUrl]
         arr.push(url)
         localStorage.setItem('arr', JSON.stringify(arr));
     } else {
         let stored = JSON.parse(localStorage.getItem('arr'));
-        let url2 = [pageUrl];
+        let url2 = [newUrl];
         stored.push(url2);
         localStorage.setItem('arr', JSON.stringify(stored));
     }
     showHighScores();
 }
 
+function deleteUrl (newUrl) {
+    let stored = JSON.parse(localStorage.getItem('arr'));
+    for (let i=0; i<stored.length; i++) {
+        if (stored[i][0] === newUrl) {
+            stored.splice(i, 1);
+            localStorage.setItem('arr',JSON.stringify(stored));
+            stored = JSON.parse(localStorage.getItem('arr'));
+            i = -1;
+        }
+    }
+    showHighScores();
+}
+
 function showHighScores() {
     document.getElementById('URLs').innerHTML = ''
-    let unsorted = JSON.parse(localStorage.getItem('arr'));
-    for (let i=0; i<unsorted.length; i++) {
-        document.getElementById('URLs').innerHTML += "<p>"+unsorted[i]+"</p>";
+    let stored = JSON.parse(localStorage.getItem('arr'));
+    for (let i=0; i<stored.length; i++) {
+        document.getElementById('URLs').innerHTML += "<p style='font-size:150%;'>"+stored[i]+"</p>";
     }
 }
