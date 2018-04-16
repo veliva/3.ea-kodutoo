637fe26a3cbfcb63763c467b683d192b753019e2
Veli Vaigur <31840289+veliva@users.noreply.github.com>
2018-04-16 15:52:16 +0300

    done
    funktsioonide ja muutujate nimed vastavad programmile.

--- options.js
+++ options.js
@@ -1,59 +1,59 @@
-window.onload = function(){
-    document.getElementById("addBtn").addEventListener("click", addURLstorage);
-    showHighScores();
-    document.getElementById("clearAllBtn").addEventListener("click", clearStorage);
-    document.getElementById("removeBtn").addEventListener("click", removeURLstorage);
-};
+window.onload = function () {
+    document.getElementById("addBtn").addEventListener("click", addURLstorage)
+    showAllURLs()
+    document.getElementById("clearAllBtn").addEventListener("click", clearStorage)
+    document.getElementById("removeBtn").addEventListener("click", removeURLstorage)
+}
 
 function clearStorage () {
-    window.localStorage.clear();
-    showHighScores();
+    window.localStorage.clear()
+    showAllURLs()
 }
 
-function addURLstorage(){
-    let newUrl = document.getElementById("addURL").value;
-    saveScore(newUrl);
+function addURLstorage () {
+    let newUrl = document.getElementById("addURL").value
+    saveURL(newUrl)
 }
 
-function removeURLstorage(){
-    let newUrl = document.getElementById("removeURL").value;
-    deleteUrl(newUrl);
+function removeURLstorage () {
+    let newUrl = document.getElementById("removeURL").value
+    deleteUrl(newUrl)
 }
 
-function saveScore (newUrl) {
-    arr = []
+function saveURL (newUrl) {
+    let arr = []
     if (window.localStorage.length == 0) {
-        url = [newUrl]
+        let url = [newUrl]
         arr.push(url)
-        localStorage.setItem('arr', JSON.stringify(arr));
+        localStorage.setItem('arr', JSON.stringify(arr))
     } else {
-        let stored = JSON.parse(localStorage.getItem('arr'));
-        let url2 = [newUrl];
-        stored.push(url2);
-        localStorage.setItem('arr', JSON.stringify(stored));
+        let stored = JSON.parse(localStorage.getItem('arr'))
+        let url = [newUrl]
+        stored.push(url)
+        localStorage.setItem('arr', JSON.stringify(stored))
     }
     document.getElementById('addURL').value = ''
-    showHighScores();
+    showAllURLs()
 }
 
 function deleteUrl (newUrl) {
-    let stored = JSON.parse(localStorage.getItem('arr'));
+    let stored = JSON.parse(localStorage.getItem('arr'))
     for (let i=0; i<stored.length; i++) {
         if (stored[i][0] === newUrl) {
-            stored.splice(i, 1);
-            localStorage.setItem('arr',JSON.stringify(stored));
-            stored = JSON.parse(localStorage.getItem('arr'));
-            i = -1;
+            stored.splice(i, 1)
+            localStorage.setItem('arr',JSON.stringify(stored))
+            stored = JSON.parse(localStorage.getItem('arr'))
+            i = -1
         }
     }
     document.getElementById('removeURL').value = ''
-    showHighScores();
+    showAllURLs()
 }
 
-function showHighScores() {
+function showAllURLs () {
     document.getElementById('URLs').innerHTML = ''
-    let stored = JSON.parse(localStorage.getItem('arr'));
+    let stored = JSON.parse(localStorage.getItem('arr'))
     for (let i=0; i<stored.length; i++) {
-        document.getElementById('URLs').innerHTML += "<p style='font-size:150%;'>"+stored[i]+"</p>";
+        document.getElementById('URLs').innerHTML += "<p style='font-size:150%;'>"+stored[i]+"</p>"
     }
 }
--- popup.js
+++ popup.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', function() {
-	document.getElementById('options').addEventListener('click', function(){
-		window.open("options.html");
+document.addEventListener('DOMContentLoaded', function () {
+	document.getElementById('options').addEventListener('click', function () {
+		window.open("options.html")
 	})
 
-	document.getElementById('tabs').addEventListener('click', function(){
-		let stored = JSON.parse(localStorage.getItem('arr'));
+	document.getElementById('tabs').addEventListener('click', function () {
+		let stored = JSON.parse(localStorage.getItem('arr'))
 		for (let i=0; i<stored.length; i++) {
-			let newURL = "https://" + stored[i][0];
-			chrome.tabs.create({ url: newURL });
+			let newURL = "https://" + stored[i][0]
+			chrome.tabs.create({ url: newURL })
 		}
 	})
 })
